6b2789a0e59c27536c3970d9b02a59a315fe1b77
Dong Ha <dongha.shin@mail.polimi.it>
2025-09-22 20:33:43 +0200

    Add files via upload

---
+++ challenge1.cpp
@@ -0,0 +1,15 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "/shared-folder/stb/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "/shared-folder/stb/stb_image_write.h"
+
+using namespace Eigen;
+
+// Function to convert RGB to grayscale
+int main(int argc, char* argv[]) {
+    
+  return 0;
+}
