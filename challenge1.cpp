70a713dadc35cd862984a2285bfb1d2b77d12259
GiovanniVaccaro <giovannivaccaro2@gmail.com>
2025-09-24 15:36:23 +0200

    Task 3

--- challenge1.cpp
+++ challenge1.cpp
@@ -1,6 +1,6 @@
 #include <Eigen/Dense>
 #include <iostream>
-#include <algorithm>            
+#include <algorithm>          
 #include <cstdlib>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -13,7 +13,7 @@ using namespace Eigen;
 int main(int argc, char* argv[]) {
   
     //TASK 1
-    if (argc < 2) {
+    if (argc < 2){
         std::cerr << "Usage: " << argv[0] << " <image_path>\n";
         return 1;
     }
@@ -25,6 +25,15 @@ int main(int argc, char* argv[]) {
     if (!image_data) {
         std::cerr << "Error: Could not load image " << input_image_path << "\n";
     return 1;
+    }
+
+    MatrixXd originalImg(height, width);
+
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            int idx = i * width + j;  // pixel in posizione (i,j)
+            originalImg(i, j) = static_cast<double>(image_data[idx]) / 255.0;
+        }
     }
 
     std::cout << "righe: " << height << "\ncolonne: " << width << "\n";
@@ -41,7 +50,6 @@ int main(int argc, char* argv[]) {
             noiseImg(i, j) = std::clamp((static_cast<double>(image_data[idx]) + noise(i,j)) / 255.0, 0.0, 1.0);
         }
     }
-    stbi_image_free(image_data);
 
     Matrix<unsigned char, Dynamic, Dynamic, RowMajor> noise_image(height, width);
   
@@ -54,10 +62,12 @@ int main(int argc, char* argv[]) {
     if (stbi_write_png(output_image_path1.c_str(), width, height, 1,
                      noise_image.data(), width) == 0) {
     std::cerr << "Error: Could not save grayscale image" << std::endl;
-
     return 1;
   }
 
-
-  
+    //Task 3
+    VectorXd v = Map<VectorXd>(originalImg.data(),originalImg.size());
+    VectorXd w = Map<VectorXd>(noiseImg.data(),noiseImg.size());
+    std::cout << "Dim di v: " << v.size()<< "\nDim di w: " << w.size() << std::endl;
+    std::cout << "Norma di v: " << v.norm() << std::endl;
 }
