4afd72b6dab9d786fba06d8ca5a984c439ef3269
Michele <nuovotempo@MacBook-Air.local>
2025-10-25 16:17:01 +0200

    end

--- challenge2/challenge2.cpp
+++ challenge2/challenge2.cpp
@@ -53,13 +53,11 @@ void countAndSort(int &neg, int &pos, int dim, vector<pair<int, double>> &v){
                 }
             }
         }
-
     }
 }
 
 int main(){
     // Task 1
-
     int n1 = 9;
     SpMat Ag(n1, n1);
 
@@ -77,64 +75,63 @@ int main(){
     Ag.coeffRef(7,8) = 1.0;
 
     Ag = SpMat(Ag.transpose()) + Ag;
-    cout << "Frobenius Norm: " << sqrt(Ag.squaredNorm()) << std::endl;
+    cout << "Frobenius Norm: " << Ag.norm() << endl;
 
     // Task 2
     /* 
     Dg è una matrice diagonale a dominanza non stretta, il vettore y = 0 perchè ogni riga contiene nella posizione (i,i) la somma degli altri elementi della riga
     e gli altri elementi della riga sono invertiti di segno per l'equazione Lg = Dg - Ag
+
+    La simmetria della matric Lg è diretta conseguenza della simmetria delle matrici Ag e Dg
+    Lg è simmetrica e SEMI-definita positiva
     
-    Lg è simmetrica e SEMI-definita positiva perchè è un Laplaciano-like : è a dominanza diagonale non stretta per righe, i valori della diagonale 
-    sono tutti >0 e quelli non diagonali sono tutti < 0.
-    La molteplicità dell’autovalore 0 è il numero di componenti connesse, in questo caso avendo tutto il grafo connesso, la molteplicità è 1 solo.
+    "La molteplicità dell’autovalore 0 è il numero di componenti connesse all'interno del grafo" 
+    fonte: "A Tutorial on Spectral Clustering", Ulrike von Luxburg
+    In questo caso, avendo tutto il grafo connesso, la molteplicità è 1.
      */
 
     VectorXd vg(n1);
     vg = Ag * VectorXd::Ones(n1);
     SpMat Dg(n1, n1);
-    for (int i = 0; i < n1; ++i) Dg.coeffRef(i,i) = vg(i);
+    for (int i = 0; i < n1; i++) 
+        Dg.coeffRef(i,i) = vg(i);
 
     SpMat Lg = Dg - Ag;
+    saveMarket(Lg, "./Lg.mtx");
     VectorXd x = VectorXd::Ones(n1);
     VectorXd y = Lg * x;
-    cout << "Norm of y: "<<y.norm() << endl;
+    cout << "Norm of y: "<< y.norm() << endl;
     
     // Task 3
-    MatrixXd L = MatrixXd(Lg); // Sparse --> Dense
-
-    SelfAdjointEigenSolver<MatrixXd> saeigensolver(L); //Solver used in case of a symmetric Matrix
+    SelfAdjointEigenSolver<MatrixXd> saeigensolver(Lg);
     if (saeigensolver.info() != Success) abort();
 
     auto eigenvalues = saeigensolver.eigenvalues();
     auto eigenvectors = saeigensolver.eigenvectors();
-    cout << "Min eigenvalue: " << eigenvalues.minCoeff() << endl << "Max Eigenvalue: " << eigenvalues.maxCoeff() << endl;
+    cout << "Min eigenvalue: " << eigenvalues.head(1) << endl << "Max Eigenvalue: " << eigenvalues.tail(1) << endl;
+
     
     // Task 4
     // in questo caso gli autovalori sono ordinati in ordine crescente e abbiamo preso l'autovettore dell'autovalore con il secondo valore minore, è coerente con la specifica
-    const double lambda = eigenvalues(1);
-    VectorXd fielder = eigenvectors.col(1);
-
-    cout << "Smallest strictly positive eigenvalue of Lg: " << lambda << endl;
-    cout << "EigenVector associated to the eigenvalue: " << endl << fielder << endl;
+    cout << "Smallest strictly positive eigenvalue of Lg: " << eigenvalues(1) << endl;
+    cout << "Eigenvector associated to the smallets strictly positive eigenvalue: " << endl << eigenvectors.col(1) << endl;
 
     // Task 5
     SpMat As; 
     loadMarket(As, "./social.mtx");
-    double frobNormS = std::sqrt(As.squaredNorm());
-    cout << "Frobenius Norm: " << frobNormS << endl;
+    cout << "Frobenius norm of As: " << As.norm() << endl;
 
     // Task 6 
     int ns = As.rows(); 
     VectorXd vs(ns);
-
     vs = As * VectorXd::Ones(ns);
     SpMat Ds(ns, ns);
-    Ds.reserve(VectorXd::Ones(ns));
-    for (int i = 0; i < ns; ++i) Ds.coeffRef(i,i) = vs(i);
+    for (int i = 0; i < ns; i++) 
+        Ds.coeffRef(i,i) = vs(i);
 
     SpMat Ls = Ds - As; 
     SpMat B = SpMat(Ls.transpose()) - Ls;
-    cout << ((B.norm() < 1e-10) ? "is Symmetric" : "its not symmetric") << endl << "nnz(Ls): " << Ls.nonZeros() << endl;
+    cout << "Ls symmetric: " << ((B.norm() < 1e-16) ? "true" : "false") << endl << "nnz(Ls): " << Ls.nonZeros() << endl;
 
     // Task 7
     // mpirun -n 4 ./eigen1 Ls.mtx VecChallenge2.txt HisChallenge2.txt -e pi -emaxiter 3000 -etol 1e-08
@@ -146,9 +143,10 @@ int main(){
     // Task 8
     // mu = 29.55
     // Iteration Count : 1063
+    // if mu > 30 ==> the calculated eigenvalue is the smallest one
 
     // Task 9 
-    // Compute Eigenvalue = 1.789090
+    // Compute Eigenvalue = 1.789070e+00
     // Corrispettive EigenVector -> VecChallenge2.mtx
     // N iterations: 113 - 4
     // Prima abbiamo trovato il secondo autovalore più piccolo con etest5
@@ -161,20 +159,20 @@ int main(){
     eigV.reserve(ns); 
     ifstream input("./lis-2.1.10/test/VecChallenge2.mtx");
     if (!input) {
-        cerr << "Error opening files\n"; 
+        cerr << "Error opening file\n"; 
         return 1; 
     }
 
     int position; 
     double val; 
 
-    string dummy;
-    getline(input, dummy);  
-    getline(input, dummy);  
-    while (input >> position >> val) {
+    string boing;
+    getline(input, boing);  
+    getline(input, boing);  
+    while (input >> position >> val){
         eigV.emplace_back(position, val);
     }
-
+    
     int nn = 0, np = 0; 
     countAndSort(nn, np, ns, eigV); 
     cout << "np: " << np << endl; 
